0785236d64e84c9a9a2d30fc6e9a1c920383996a
toosyou <toosyou.tw@gmail.com>
2017-03-17 05:40:58 +0800

    finished reading addresses

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -3,18 +3,29 @@
 #include <vector>
 #include <arpa/inet.h>
 #include <cstdlib>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
+#define ADDRESS_TCP "/proc/net/tcp"
+#define ADDRESS_TCP6 "/proc/net/tcp6"
+#define ADDRESS_UDP "/proc/net/udp"
+#define ADDRESS_UDP6 "/proc/net/udp6"
+
 struct proc_net{
     bool ipv4;
+    bool tcp;
     char local_ip[200];
     int local_port;
     char remote_ip[200];
     int remote_port;
+    int uid;
+    int inode;
 
-    proc_net(bool is_ipv4 = true){
+    proc_net(bool is_ipv4 = true, bool is_tcp=true){
         this->ipv4 = is_ipv4;
+        this->tcp = is_tcp;
     }
 
     char* _read_ip(char* ip_str, char* output_str, bool is_ipv4 = true){
@@ -56,15 +67,153 @@ struct proc_net{
         return;
     }
 
+    void read_local_ip_port(char* ip_port_str){
+        char *dontcare = NULL;
+        char *ip_str = strtok(ip_port_str, ":");
+        char *port_str = strtok(NULL, ":");
+        this->read_local_ip(ip_str);
+        this->local_port = strtol(port_str, &dontcare, 16);
+    }
+
+    void read_remote_ip_port(char* ip_port_str){
+        char *dontcare = NULL;
+        char *ip_str = strtok(ip_port_str, ":");
+        char *port_str = strtok(NULL, ":");
+        this->read_remote_ip(ip_str);
+        this->remote_port = strtol(port_str, &dontcare, 16);
+    }
 
 };
 
+struct program{
+    int PID;
+    char name[200];
+};
+
+int number_of_digits(int input){
+    int number_of_digits = 0;
+
+    do {
+        ++number_of_digits;
+        input /= 10;
+    } while (input);
+
+    return number_of_digits;
+}
+
+proc_net read_one_net(fstream &fin, bool ipv4=true, bool tcp=true){
+    proc_net rtn(ipv4, tcp);
+
+    char buffer[200] = {0};
+    int number_end_dontcare = tcp ? 7 : 3;
+
+    fin >> buffer; //local_address:port
+    rtn.read_local_ip_port(buffer);
+    fin >> buffer; //rem_address:port
+    rtn.read_remote_ip_port(buffer);
+    for(int i=0;i<4;++i)
+        fin >> buffer; // dontcare
+    fin >> rtn.uid;
+    fin >> buffer; //timeout
+    fin >> rtn.inode;
+    for(int i=0;i<number_end_dontcare;++i)
+        fin >> buffer; //dontcare
+
+    return rtn;
+}
+
+vector<proc_net> read_net(const char* address_net, bool ipv4, bool tcp){
+    vector<proc_net> rtn;
+
+    char buffer[200] = {0};
+    int number_title = tcp ? 12 : 15;
+    fstream fin(address_net, fstream::in);
+    if(fin.is_open() == false)
+        return rtn;
+
+    //headline, dontcare
+    for(int i=0;i<number_title;++i){
+        fin >> buffer;
+    }
+    while(fin >> buffer){ // {sl}:
+        rtn.push_back( read_one_net(fin, ipv4, tcp) );
+    }
+    fin.close();
+
+    return rtn;
+}
+
+vector<proc_net> read_net(const char* address_net, const char* type){
+    bool ipv4 = true;
+    bool tcp = true;
+    if(strcmp(type, "tcp") == 0){
+        ipv4 = true;
+        tcp = true;
+    }
+    else if(strcmp(type, "tcp6") == 0){
+        ipv4 = false;
+        tcp = true;
+    }
+    else if(strcmp(type, "udp") == 0){
+        ipv4 = true;
+        tcp = false;
+    }
+    else if(strcmp(type, "udp6") == 0){
+        ipv4 = true;
+        tcp = false;
+    }
+    return read_net(address_net, ipv4, tcp);
+}
+
+void print_title(void){
+    cout << "Proto Local Address"<< "\t\t" << "Foreign Address" << "\t\t" << "PID/Program name and arguments" <<endl;
+    return;
+}
+
+void print_net(vector<proc_net> &nets){
+    for(int i=0;i<nets.size();++i){
+        if(nets[i].tcp)
+            cout << "tcp";
+        else
+            cout << "udp";
+        if(nets[i].ipv4 == false)
+            cout << "6";
+        else
+            cout << " ";
+        cout << "  ";
+        cout << nets[i].local_ip << ":" << nets[i].local_port << "\t\t";
+        // ip:port is too short
+        if(strlen(nets[i].local_ip) + number_of_digits(nets[i].local_port) < 8)
+            cout << "\t";
+
+        cout << nets[i].remote_ip << ":";
+        if(nets[i].remote_port != 0)
+            cout << nets[i].remote_port << "\t\t";
+        else
+            cout << "*" << "\t\t";
+        cout <<endl;
+    }
+    return;
+}
+
 int main(int argc, char *argv[]){
 
-    proc_net tcp6(false);
-    tcp6.read_local_ip(argv[1]);
+    vector<proc_net> tcps = read_net(ADDRESS_TCP, "tcp");
+    vector<proc_net> tcp6s = read_net(ADDRESS_TCP6, "tcp6");
+    vector<proc_net> udps = read_net(ADDRESS_UDP, "udp");
+    vector<proc_net> udp6s = read_net(ADDRESS_UDP6, "udp6");
 
-    printf("%s\n", tcp6.local_ip);
+
+    cout << "List of TCP connections:" <<endl;
+    print_title();
+    print_net(tcps);
+    print_net(tcp6s);
+
+    cout << endl;
+    cout << "List of UDP connections:" <<endl;
+    print_title();
+    print_net(udps);
+    print_net(udp6s);
 
     return 0;
 }
